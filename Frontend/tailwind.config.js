490a5cace70c0860ff6ea6d4440ed5b751161b87
Srinivas lingampelli <146204265+lingampellisri@users.noreply.github.com>
2024-09-01 16:32:25 -0400

    Add files via upload

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.mjs
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config;
